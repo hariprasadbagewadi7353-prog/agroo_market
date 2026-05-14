33e21166cbf67bcdefb9f918d74d1ab6276fb9ce
Hariprasad Bagewadi <hariprasadbagewadi7353@gmail.com>
2026-05-14 10:57:07 +0530

    new update

--- app.py
+++ app.py
@@ -670,6 +670,131 @@ def logout():
     return redirect('/')
 
 
+
+# ------------------------
+# VIEW ALL FARMERS
+# ------------------------
+
+@app.route('/all_farmers')
+def all_farmers():
+
+    farmers = Farmer.query.all()
+
+    data = []
+
+    for f in farmers:
+
+        data.append({
+
+            "id": f.id,
+            "username": f.username
+
+        })
+
+    return {"farmers": data}
+
+
+# ------------------------
+# VIEW ALL CUSTOMERS
+# ------------------------
+
+@app.route('/all_customers')
+def all_customers():
+
+    customers = Customer.query.all()
+
+    data = []
+
+    for c in customers:
+
+        data.append({
+
+            "id": c.id,
+            "username": c.username
+
+        })
+
+    return {"customers": data}
+
+
+# ------------------------
+# VIEW ALL PRODUCTS
+# ------------------------
+
+@app.route('/all_products')
+def all_products():
+
+    products = Product.query.all()
+
+    data = []
+
+    for p in products:
+
+        data.append({
+
+            "id": p.id,
+            "name": p.name,
+            "price": p.price,
+            "location": p.location,
+            "quantity": p.quantity,
+            "image": p.image
+
+        })
+
+    return {"products": data}
+
+
+# ------------------------
+# VIEW ALL ORDERS
+# ------------------------
+
+@app.route('/all_orders')
+def all_orders():
+
+    orders = Order.query.all()
+
+    data = []
+
+    for o in orders:
+
+        data.append({
+
+            "id": o.id,
+            "customer_name": o.customer_name,
+            "product_name": o.product_name,
+            "address": o.address,
+            "status": o.status
+
+        })
+
+    return {"orders": data}
+
+
+# ------------------------
+# VIEW ALL SCHEMES
+# ------------------------
+
+@app.route('/all_schemes')
+def all_schemes():
+
+    schemes = Scheme.query.all()
+
+    data = []
+
+    for s in schemes:
+
+        data.append({
+
+            "id": s.id,
+            "title": s.title,
+            "description": s.description,
+            "image": s.image,
+            "video": s.video
+
+        })
+
+    return {"schemes": data}
+
 # ------------------------
 # RUN
 # ------------------------
